b5e883949dfa49f9f40aefdd8e1003c9910d4410
daylon <daylonjhons@daylons-MBP.home>
2024-01-17 22:20:26 -0600

    added to questions to questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed within:",
+        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        answer: "parenthesis"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store:",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed when being assigned to variables with:",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4 console.log"],
+        answer: "console.log"
+    }
+];
